cad15c86f7d823f190c11c023bbc9464fa7608cf
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-12-06 11:21:36 -0600

    Add CSharp4FeaturesDemo project

--- CSharp4FeaturesDemo/Program.cs
+++ CSharp4FeaturesDemo/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp4FeaturesDemo
+{
+
+// Example class to demonstrate dynamic binding
+    public class DynamicExample
+    {
+
+    }
+
+    // Interface to demonstrate covariance
+    public interface IAnimal<out T>
+    {
+
+    }
+
+    // Covariant implementation
+    public class Dog : IAnimal<Dog>
+    {
+
+    }
+
+    public class Cat : IAnimal<Cat>
+    {
+
+    }
+
+    // Main Program
+    class Program
+    {
+        // Method to demonstrate named and optional parameters
+        public static void DisplayInfo(string message, string prefix = "Info", int repeat = 1)
+        {
+
+        }
+
+        // Method demonstrating covariance
+        public static void PrintAnimal(IAnimal<object> animal)
+        {
+
+        }
+
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!");
+
+        }
+
+    }
+}
