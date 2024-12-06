f2270cdf0f35eb15f1071b7b8806d8b0cca0b259
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-12-05 18:00:52 -0600

    Creating first structure

--- CoreObjectOrientedFeatures/Program.cs
+++ CoreObjectOrientedFeatures/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Namespace encapsulating the entire application
+namespace CoreObjectOrientedFeatures
+{
+    // Abstract Class (base for all animals)
+    public abstract class Animal
+    {
+
+    }
+
+    // Interface defining behavior for flying animals
+    public interface IFlyable
+    {
+
+    }
+
+    // Derived Class for Dog
+    public class Dog : Animal
+    {
+
+    }
+
+    // Derived Class for Bird implementing an interface
+    public class Bird : Animal, IFlyable
+    {
+
+    }
+
+    // Another Interface for aquatic animals
+    public interface ISwimmable
+    {
+
+    }
+
+    // Derived Class for Fish implementing an interface
+    public class Fish : Animal, ISwimmable
+    {
+
+    }
+
+    // Main Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
