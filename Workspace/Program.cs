420f3b19ff2024fdaaf5d19e3debc7ee254dacc2
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-12-07 18:56:49 -0600

    Showing Abstract class behavior using abstract property and method

--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -5,6 +5,9 @@ using MyProject;
 using MyProject.Utilities;
 using MyProject.Models;
 using AliasModels = MyProject.Models;
+
+#region Namespaces
+
 //???
 //A using clause must precede all other elements defined in the namespace except extern alias declarations
 
@@ -22,6 +25,10 @@ using AliasModels = MyProject.Models;
 
 //namespaces System, System.Collections.Generic, System.IO, System.Threading etc
 
+//Makes it easy to breake code into manageable chunks
+//Ideal for large projects where organization is key.
+//Allows multiple developers to work on the same project without name conflicts
+//Promotes the sharing of code in different projects
 
 class Program{
     static void Main(){
@@ -51,6 +58,13 @@ class Program{
         Console.WriteLine(objAliasProduct.Name);
         Console.WriteLine(objAliasUser.Name);
 
+        Circle circle = new Circle();
+        circle.Name = "Circle";
+        circle.CalculateArea();
+        Console.WriteLine(circle.Name);
+        Console.WriteLine( circle.CalculateArea());
+
+
     }
 }
 
@@ -78,3 +92,37 @@ namespace MyProject
 }
 
 
+
+#endregion
+
+#region AbstractClasses
+//It cannot be instantiated directly
+//You can only use it as a base class
+abstract class Figure{
+    //Abstract properties have no body
+    public abstract string Name { get; set;} // Abstract property
+    //Non-abstract properties have body
+    public int Sides { get; set; } = 4; // Non-abstract property
+    // Abstract methods have no implementation and must be implemented
+    // by derived classes
+    public abstract double CalculateArea();
+    // Non-abstract methods have implementation and can be inherited directly
+    public void ShowMessage(){
+        Console.WriteLine("I am a figure");
+    }
+}
+
+class Circle: Figure {
+     //Derived classes must provide implementations for all abstract members
+     public override global::System.String Name { 
+        get => throw new System.NotImplementedException(); 
+        set => throw new System.NotImplementedException(); 
+    }
+     
+     //error CS0534: 'Circle' does not implement inherit ted abstract member 'Figure.Name.get'
+     public override global::System.Double CalculateArea() => throw new System.NotImplementedException();
+ }
+
+//You can´t do this 
+//Figure myFigure = new Figure(); //Error
+#endregion
