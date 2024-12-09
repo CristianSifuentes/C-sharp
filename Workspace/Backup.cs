a04853e6c1ff5e95bc400febae6c6584971bbb4d
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-12-09 10:48:52 -0600

    Add code using interface

---
+++ Workspace/Backup.cs
@@ -0,0 +1,208 @@
+using System;
+using System.Collections.Generic;
+using MyProject;
+using MyProject.Utilities;
+using MyProject.Models;
+using AliasModels = MyProject.Models;
+
+#region Namespaces
+
+//???
+//A using clause must precede all other elements defined in the namespace except extern alias declarations
+
+//namespace is a way to organize and group classes, 
+//interfaces, structures, enumerations, delegates, and 
+//other code elements.
+
+//if two developers create classes with the same name in different parts
+//using namespaces correctly you can avoid conflicts
+
+//Using namespaces you can reuse code in different parts
+
+//Namespaces can be nested which allows for a hierarchical structure
+//from an application or library
+
+//namespaces System, System.Collections.Generic, System.IO, System.Threading etc
+
+//Makes it easy to brake code into manageable chunks
+//Ideal for large projects where organization is key.
+//Allows multiple developers to work on the same project without name conflicts
+//Promotes the sharing of code in different projects
+
+class Program{
+    static void Main(){
+        MyClass obj = new MyClass();
+        obj.ShowInfo();
+        Tools tool = new Tools();
+        tool.Hi();
+        User user = new User();
+        user.Name = "Artur";
+        Product product = new Product();
+        product.Name = "Apple";
+
+        Console.WriteLine(product.Name);
+        Console.WriteLine(user.Name);
+
+        List<string> names = new List<string>{
+            "Ana", "Marie", "Sofia"
+        };
+        foreach (var name in names){
+            Console.WriteLine(name);
+        }
+        
+        AliasModels.Product objAliasProduct = new AliasModels.Product();
+        AliasModels.User objAliasUser = new AliasModels.User();
+        objAliasProduct.Name = "PC";
+        objAliasUser.Name = "Jose";
+        Console.WriteLine(objAliasProduct.Name);
+        Console.WriteLine(objAliasUser.Name);
+
+        //error CS1503: Argument 1: cannot convert from 'string' to 'double'
+        Circle circle = new Circle(8);
+        circle.Name = "Circle";
+        circle.CalculateArea();
+        Console.WriteLine(circle.Name);
+        Console.WriteLine(circle.CalculateArea());
+
+
+    }
+
+}
+
+namespace MyProject{
+    namespace Utilities{
+        class Tools{
+            public void Hi(){
+               Console.WriteLine("Hi from Utilities.Tools");
+ 
+            }
+        }
+    }
+}
+
+namespace MyProject
+{
+    class MyClass
+    {
+        public void ShowInfo(){
+            Console.WriteLine("Hi fom MyClass in MyProject");
+        }
+    }
+}
+
+#endregion
+
+#region AbstractClasses
+//It cannot be instantiated directly
+//You can only use it as a base class
+//Code reuse: You can define common behavior in the abstract class and extend it in derived classes
+//Consistency: Ensures that all derived classes implement certain methods and properties.
+//Flexibility: It allow you to define a base structure without specifying all the details, leaving room for customization int derived classes
+
+
+abstract class Shape{
+    //Abstract properties have no body
+    public abstract string Name { get; set;} // Abstract property
+    //Non-abstract properties have body
+    public int Sides { get; set; } = 4; // Non-abstract property
+    public double Radius { get; set; } = 0.0;
+
+    // Abstract methods have no implementation and must be implemented
+    // by derived classes
+    public abstract double CalculateArea();
+    // Non-abstract methods have implementation and can be inherited directly
+    public void ShowInfo(){
+        Console.WriteLine("I am a Shape");
+    }
+    //Abstract class 
+    //Although it cannot be instantiated, it can have a constructor that will be called by derived classes.   
+    public Shape(double radius){
+         Radius = radius;
+        //error CS7036: There is no argument given that corresponds to the required parameter 'color' of 'Shape.Shape(string)'
+    }
+}
+
+class Circle: Shape {
+    //Derived classes must provide implementations for all abstract members
+    //  public override global::System.String Name { 
+    //     get => throw new System.NotImplementedException(); 
+    //     set => throw new System.NotImplementedException(); 
+    // }
+    //error CS0102: The type 'Circle' already contains a definition for 'Name'
+    public override global::System.String Name {
+         get => throw new System.NotImplementedException(); 
+         set => throw new System.NotImplementedException(); 
+    }
+
+    // public double Radius { get; set; }
+     
+     //error CS0534: 'Circle' does not implement inherit ted abstract member 'Shape.Name.get'
+    // public override global::System.Double CalculateArea() => throw new System.NotImplementedException();
+
+    //error CS0111: Type 'Circle' already defin es a member called 'CalculateArea' with the same parameter types 
+    public override double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+     
+    //  public Circle(string color): base(color) { }
+     public Circle(double radius):base(radius) {}
+ }
+
+class Square : Shape
+{
+    public Square(double radius) : base(radius)
+    {
+    }
+
+    //error CS0534: 'Square' does not implement  inherited abstract member 'Shape.Name.get
+    public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public override double CalculateArea()
+    {
+        throw new NotImplementedException();
+    }
+}
+//You can´t do this 
+//Shape myShape = new Shape(); //Error
+#endregion
+
+namespace interfaz{
+//In C#, an interface is a contract that 
+//defines a set of methods, properties, events, 
+//or indexers that a class or structure must implement.
+//Interfaces do not contain implementation; they
+//only specify which members should be implemented.
+    #region Interfaz  
+    interface IVehicle {
+        void Star();
+        void Stop();
+    }
+    class Car : IVehicle
+    {
+        public void Star()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Motorcycle : IVehicle
+    {
+        public void Star()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    #endregion
+
+}
--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -1,172 +1,79 @@
 ﻿
-using System;
-using System.Collections.Generic;
-using MyProject;
-using MyProject.Utilities;
-using MyProject.Models;
-using AliasModels = MyProject.Models;
-
-#region Namespaces
-
-//???
-//A using clause must precede all other elements defined in the namespace except extern alias declarations
-
-//namespace is a way to organize and group classes, 
-//interfaces, structures, enumerations, delegates, and 
-//other code elements.
-
-//if two developers create classes with the same name in different parts
-//using namespaces correctly you can avoid conflicts
-
-//Using namespaces you can reuse code in different parts
-
-//Namespaces can be nested which allows for a hirarchical structure
-//from an application or library
-
-//namespaces System, System.Collections.Generic, System.IO, System.Threading etc
-
-//Makes it easy to breake code into manageable chunks
-//Ideal for large projects where organization is key.
-//Allows multiple developers to work on the same project without name conflicts
-//Promotes the sharing of code in different projects
-
-class Program{
-    static void Main(){
-        MyClass obj = new MyClass();
-        obj.ShowInfo();
-        Tools tool = new Tools();
-        tool.Hi();
-        User user = new User();
-        user.Name = "Artur";
-        Product product = new Product();
-        product.Name = "Apple";
-
-        Console.WriteLine(product.Name);
-        Console.WriteLine(user.Name);
-
-        List<string> names = new List<string>{
-            "Ana", "Marie", "Sofia"
-        };
-        foreach (var name in names){
-            Console.WriteLine(name);
-        }
-        
-        AliasModels.Product objAliasProduct = new AliasModels.Product();
-        AliasModels.User objAliasUser = new AliasModels.User();
-        objAliasProduct.Name = "PC";
-        objAliasUser.Name = "Jose";
-        Console.WriteLine(objAliasProduct.Name);
-        Console.WriteLine(objAliasUser.Name);
-
-        //error CS1503: Argument 1: cannot convert from 'string' to 'double'
-        Circle circle = new Circle(8);
-        circle.Name = "Circle";
-        circle.CalculateArea();
-        Console.WriteLine(circle.Name);
-        Console.WriteLine(circle.CalculateArea());
-
-
-    }
-}
-
-namespace MyProject{
-    namespace Utilities{
-        class Tools{
-            public void Hi(){
-               Console.WriteLine("Hi from Utilities.Tools");
- 
-            }
-        }
-    }
-}
-
-
-
-namespace MyProject
-{
-    class MyClass
-    {
+#region AbstractClassesNamespaces
+// Namespace for shapes
+namespace Geometry.Shapes{
+    public abstract class Shape{
+        // Abstract property 
+        public abstract string Name { get; }
+        // Abstract method
+        public abstract double CalculateArea();
+        //Non-abstract method
         public void ShowInfo(){
-            Console.WriteLine("Hi fom MyClass in MyProject");
+            Console.WriteLine($"I am a shape called {Name}");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public override string Name => "Circle";
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public Circle(double radius){
+            Radius = radius;
+        }
+    }
+    public class Square : Shape
+    {
+        public double Side { get; set; }
+        public override string Name => "Square";
+        
+        public Square(double side){
+            Side = side;
+        }
+
+        public override double CalculateArea()
+        {
+            return Side * Side;
         }
     }
 }
-
-
-
-#endregion
-
-#region AbstractClasses
-//It cannot be instantiated directly
-//You can only use it as a base class
-//Code reuse: You can define common behavior in the abstract class and extend it in derived classes
-//Consistency: Ensures that all derived classes implement certain methods and properties.
-//Flexibility: It allow you to define a base structure without specifying all the details, leaving room for customization int derived classes
-
-
-abstract class Shape{
-    //Abstract properties have no body
-    public abstract string Name { get; set;} // Abstract property
-    //Non-abstract properties have body
-    public int Sides { get; set; } = 4; // Non-abstract property
-    public double Radius { get; set; } = 0.0;
-
-    // Abstract methods have no implementation and must be implemented
-    // by derived classes
-    public abstract double CalculateArea();
-    // Non-abstract methods have implementation and can be inherited directly
-    public void ShowInfo(){
-        Console.WriteLine("I am a Shape");
+// Namespace for utilities
+namespace Geometry.Utilities{
+  public static class MathUtilities {
+    public static double CalculateCircleCircumference(double radius){
+        return 2 * Math.PI * radius;
     }
-    //Abstract class 
-    //Although it cannot be instantiated, it can have a constructor that will be called by derived classes.   
-    public Shape(double radius){
-         Radius = radius;
-        //error CS7036: There is no argument given that corresponds to the required parameter 'color' of 'Shape.Shape(string)'
+    public static double CalculateSquarePerimeter(double side){
+        return 4 * side;
     }
+  }
 }
-
-class Circle: Shape {
-    //Derived classes must provide implementations for all abstract members
-    //  public override global::System.String Name { 
-    //     get => throw new System.NotImplementedException(); 
-    //     set => throw new System.NotImplementedException(); 
-    // }
-    //error CS0102: The type 'Circle' already contains a definition for 'Name'
-    public override global::System.String Name {
-         get => throw new System.NotImplementedException(); 
-         set => throw new System.NotImplementedException(); 
-    }
-
-    // public double Radius { get; set; }
-     
-     //error CS0534: 'Circle' does not implement inherit ted abstract member 'Shape.Name.get'
-    // public override global::System.Double CalculateArea() => throw new System.NotImplementedException();
-
-    //error CS0111: Type 'Circle' already defin es a member called 'CalculateArea' with the same parameter types 
-    public override double CalculateArea()
-    {
-        return Math.PI * Radius * Radius;
-    }
-     
-    //  public Circle(string color): base(color) { }
-     public Circle(double radius):base(radius) {}
- }
-
-class Square : Shape
+// Main Program
+namespace GeometryApp
 {
-    public Square(double radius) : base(radius)
-    {
-    }
+    using Geometry.Shapes;
+    using Geometry.Utilities;
 
-    //error CS0534: 'Square' does not implement  inherited abstract member 'Shape.Name.get
-    public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-    public override double CalculateArea()
+    class Program1
     {
-        throw new NotImplementedException();
+        static void Main()
+        {
+            Shape circle = new Circle(5);
+            Shape square = new Square(4);
+
+            // Display shape info and areas
+            circle.ShowInfo();
+            Console.WriteLine($"Area: {circle.CalculateArea()}");
+            Console.WriteLine($"Circumference: {MathUtilities.CalculateCircleCircumference(5)}");
+
+            square.ShowInfo();
+            Console.WriteLine($"Area: {square.CalculateArea()}");
+            Console.WriteLine($"Perimeter: {MathUtilities.CalculateSquarePerimeter(4)}");
+        }
     }
 }
-//You can´t do this 
-//Shape myShape = new Shape(); //Error
 #endregion
