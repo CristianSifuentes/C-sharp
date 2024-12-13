8cf709f94fa6a537c644c6d9fb35c435a4f1f953
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-12-13 12:41:41 -0600

    Knowledge about generic class vs normal class, const vs readonly

--- GenericsDemo/Program.cs
+++ GenericsDemo/Program.cs
@@ -4,8 +4,23 @@ using System.Collections.Generic;
 // Namespace encapsulating the application
 namespace GenericsDemo
 {
+
+    /*
+    It is designed to work with specific data 
+    types explicitly defined in its implementation.
+    If you need to work with different types, you must create
+    multiple versions of the same class or use inheritance.
+    
+    It allows you to work with any type of data without
+    having to write multiple version of the same class.
+    It uses a generic type parameter that is
+    defined with <T> and can be replaced by
+    any type when the class is instantiated.
+    Promotes code reusability and flexibility.
+
+    */
     // Generic Class Example
-    public class GenericRepository<W>
+    public class GenericRepository<T>: IRepository<T>
     {
         //class System.Collections.Generic.List<T>
         //readonly is used to declare fields that can only be
@@ -22,32 +37,58 @@ namespace GenericsDemo
         The value must be known at compile time.
         */
 
-        private readonly List<W> _items = new();
+        private readonly List<T> _items = new();
         public readonly int ReadOnlyField; // readonly field
-        public const int ConstField = 100; // Campo const
-        public readonly DateTime ReadOnlyDate; // Campo readonly para un tipo de referencia
+        public const int ConstField = 100; // const field 
+        public readonly DateTime ReadOnlyDate; // readonly field for a reference type
         
-
-
-        // Add item to the repository
-        // Change T for W
-        public void Add(W item)
-        {
-            _items.Add(item);
+        private readonly string _connectionString;
+        private const string TableName = "Entities";
+        
+        /*
+        CS8618: Non-nullable field '_connectionString' must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring the field as nullable.
+        */
+        public GenericRepository(string connectionString){
+            _connectionString = connectionString;
         }
 
-        // interface System.Collections.Generic.IEnumerable<out T>
+                // interface System.Collections.Generic.IEnumerable<out T>
         // Get all items
-        public IEnumerable<W> GetAll()
+        public IEnumerable<T> GetAll()
         {
+            Console.WriteLine($"Fetching all records from table {TableName} using {_connectionString}");
             return _items;
+        }
+
+        public T GetById(int id)
+        {
+            Console.WriteLine($"Fetching record with ID {id} from table {TableName}");
+            // throw new NotImplementedException();
+            return default(T);
+        }
+        // Add item to the repository
+        // Change T for W
+        public void Add(T entity)
+        {
+            Console.WriteLine($"Adding new record to table {TableName}");
+            _items.Add(entity);
         }
 
         // Generic method to find an item by predicate
         //delegate bool System.Predicate<in T>(T obj)
-        public W Find(Predicate<W> match)
+        public T Find(Predicate<T> match)
         {
             return _items.Find(match);
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 
@@ -56,6 +97,15 @@ namespace GenericsDemo
     {
         bool Compare(T other);
     }
+    
+    public interface IRepository<T>{
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+    
 
     // Example of implementing a generic interface
     public class Product : IComparableEntity<Product>
@@ -74,13 +124,57 @@ namespace GenericsDemo
         }
     }
 
+    public class ReadOnlyExample{
+        public readonly int ReadOnlyField;
+        public const int ConstField = 100;
+        public readonly DateTime ReadOnlyDate;
+
+        public ReadOnlyExample(int initialValue){
+            ReadOnlyField = initialValue;
+            ReadOnlyDate = DateTime.Now;
+        }
+        public void DisplayValues(){
+            Console.WriteLine($"ReadOnlyField: {ReadOnlyField}");
+            Console.WriteLine($"ConstField: {ConstField}");
+            Console.WriteLine($"ReadOnlyDate: {ReadOnlyDate}");
+        }
+    }
+
     // Program demonstrating the use of generics
     class Program
     {
         static void Main(string[] args)
         {
+            string connectionString = "Server=myServer;Database=myDb;User=myUser;Password=myPassword;";
+
+            var example = new ReadOnlyExample(42);
+
+            example.DisplayValues();
+            
+            //CS0191: A readonly field cannot be assigned to 
+            //(except in a constructor or init-only setter of the type 
+            //in which the field is defined or a variable initializer)
+            // example.ReadOnlyField = 50;
+            
+            // CS0131: The left-hand side of an assignment must be a variable, property or indexer
+
+            // ReadOnlyExample.ConstField = 200;
+             
+            /*
+            However, the internal properties of an object referenced by 
+            readonly field can change:
+            ReadOnlyDate is struct (immutable), so it has no internal properties
+            that can be modified. 
+            */ 
+            /*
+            CS0191: A readonly field cannot be assigned to (except in a constructor or init-only setter of the type in which the field is defined or a variable initializer)
+            */
+            // example.ReadOnlyDate = DateTime.Now;
+            example.ReadOnlyDate.AddHours(1);
+
             // Using GenericRepository with integers
-            var intRepo = new GenericRepository<int>();
+            var intRepo = new GenericRepository<int>(connectionString);
+
             intRepo.Add(1);
             intRepo.Add(2);
             intRepo.Add(3);
@@ -92,7 +186,7 @@ namespace GenericsDemo
             }
 
             // Using GenericRepository with custom type (Product)
-            var productRepo = new GenericRepository<Product>();
+            var productRepo = new GenericRepository<Product>(connectionString);
             productRepo.Add(new Product { Name = "Laptop", Price = 1500.00m });
             productRepo.Add(new Product { Name = "Mouse", Price = 25.00m });
 
