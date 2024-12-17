9f377692b84895ca9656f9cb9a45ddcc3b7b6336
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-12-17 17:33:53 -0600

    Creating example for show System.Collections, extension method, LINQ

--- CSharp3FeaturesDemo/Program.cs
+++ CSharp3FeaturesDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -48,6 +49,49 @@ namespace CSharp3FeaturesDemo
                 new Product { Name = "Monitor", Price = 300.00m },
                 new Product { Name = "USB Cable", Price = 10.00m }
             };
+
+            // Using ArrayList to demonstrate compatibility with System.Collections
+            ArrayList taskList = new ArrayList {
+               new Task { Id = 1, Name = "Fix Bug", Priority = "High", CreatedAt = DateTime.Now.AddDays(-1) },
+               new Task { Id = 2, Name = "Code Review", Priority = "Medium", CreatedAt = DateTime.Now.AddDays(-2) },
+               new Task { Id = 3, Name = "Write Documentation", Priority = "Low", CreatedAt = DateTime.Now.AddDays(-5) },
+               new Task { Id = 4, Name = "Deploy App", Priority = "High", CreatedAt = DateTime.Now.AddDays(-10) }
+            };
+            
+            // Convert ArrayList to IEnumerable<Task> to using LINQ and extension method ??
+            IEnumerable<Task> tasks = taskList.Cast<Task>();
+            
+            // Use LINQ to count task by priority
+            var taskCountByPriority = tasks.GroupBy(t=> t.Priority).Select(g => new {  Priority = g.Key, Count = g.Count() });
+            
+            Console.WriteLine("Task Count by Priority");
+            foreach(var item in taskCountByPriority){
+                Console.WriteLine($"{item.Priority}: {item.Count}");
+            }
+
+            //Use extension method to filter high priority task
+            var highPriorityTasks = tasks.FilterByPriority("High");
+            Console.WriteLine("\nHigh Priority Tasks:");
+            foreach (var task in highPriorityTasks)
+            {
+                Console.WriteLine(task);
+            }
+            // Use extension method to get recent task (last 3 days)
+            var recentTasks = tasks.GetRecentTasks(3);
+            Console.WriteLine("\nRecent Tasks (Last 3 Days):");
+            foreach (var task in recentTasks)
+            {
+                Console.WriteLine(task);
+            }
+            // Use lambda expression directly filter old task
+            var oldTasks = tasks.Where(t => t.CreatedAt < DateTime.Now.AddDays(-7));
+
+            Console.WriteLine("\nOld Tasks (Older than 7 Days):");
+
+            foreach (var task in oldTasks)
+            {
+                Console.WriteLine(task);
+            }
 
             // LINQ Query to filter products by price
             var expensiveProducts = from p in products
